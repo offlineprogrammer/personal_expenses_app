15d80244c2230e49af2fe0061a06358ed5a117cb
offlineProgrammer <mmalaka@gmail.com>
2020-12-24 18:12:48 -0800

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -120,7 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction)
+            TransactionList(_userTransactions, _deleteTransaction),
           ],
         ),
       ),
